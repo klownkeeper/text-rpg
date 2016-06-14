3688d5b41c9da2d8577e77afe2f94f93dd7ac170
Meng Zhao <meng.zhao@rakuten.com>
2016-06-14 16:26:17 +0900

    update

--- battle.py
+++ battle.py
@@ -1,5 +1,14 @@
+import random
+
 TURN_CONST = 10.0
 
+def dice(dice_str):
+    n, d = map(int, dice_str.split('d'))
+    res = 0
+    for i in range(n):
+        res += random.choice(range(1, d+1))
+    return res
+    
 
 class Unit(object):
     id = None
@@ -8,11 +17,19 @@ class Unit(object):
 
     unit_hp = 10
     unit_mp = 10
-    unit_attack = 1
-    unit_defence = 1
+    unit_damage = "1d2"
+    unit_attack = 0
+    unit_defence = 0
 
-    def action(self, situation):
-        # print("Action", self.name, situation)
+    action_list = {
+        "normal_attack":{
+            'unit_attack': 0,
+            'unit_damage': '1d2',
+            'unit_cooldown': TURN_CONST,
+            },
+    }
+
+    def action(self, world):
         print("unit %s, HP:%d" % (self.name, self.unit_hp))
 
 
@@ -20,17 +37,38 @@ class TeamedUnit(Unit):
     team = None
 
     def action(self, situation):
+        raise NotImplementedError
         # print("Action", self.name, situation)
         enemy_list = []
         for idx in range(len(situation)):
-            print(situation, idx)
             if situation[idx].team != self.team:
                 enemy_list.append(idx)
         if not enemy_list:
-            print("enemy_list empty")
             return
-        situation[enemy_list[0]].unit_hp -= 1
+        damage = dice(self.unit_damage) + self.unit_attack - situation[enemy_list[0]].unit_defence
+        print(damage)
+        situation[enemy_list[0]].unit_hp -= damage
         print("unit %s, HP:%d" % (self.name, self.unit_hp))
+
+
+class Creature(TeamedUnit):
+    """docstring for Abstract Creature"""
+
+    def enemy_list(self, world):
+        enemy_list = []
+        for idx in range(len(world.unit_list)):
+            if world.unit_list[idx].team != self.team:
+                enemy_list.append(idx)
+        if not enemy_list:
+            raise "Battle is already finished!"
+        return enemy_list
+
+    def choose_action(self, world):
+        return "normal_attack", self.enemy_list(world)[0]
+
+    def action(self, world):
+        action, target_idx = self.choose_action(world)
+        world.attack(world.unit_list.index(self), target_idx, action)
 
 
 class Situation(object):
@@ -69,7 +107,7 @@ class ActionManager(object):
         if not self.ready:
             return self.ready
         for idx in self.unit_ready_list:
-            self.unit_list[idx].action(self.situation())
+            self.unit_list[idx].action(self)
             self.cooldown_list[idx] = TURN_CONST
         self.unit_ready_list = []
         self.ready = False
@@ -93,16 +131,32 @@ class ActionManager(object):
                 self.unit_ready_list.append(idx)
 
 
+class DamageManager(ActionManager):
+
+    def attack(self, attacker_id, defencer_id, attack_name):
+        attacker = self.unit_list[attacker_id]
+        defencer = self.unit_list[defencer_id]
+        damage = attacker.action_list[attack_name]['unit_damage']
+        attack = attacker.action_list[attack_name]['unit_attack']
+        defenc = defencer.unit_defence
+        damage_deal = dice(damage) + attack - defenc
+        if damage_deal < 0:
+            damage_deal = 0
+        self.unit_list[defencer_id].unit_hp -= damage_deal
+        print("* %s %s %s deal %d points damage: (%d)" % (
+            attacker.name, attack_name, defencer.name, damage_deal, self.unit_list[defencer_id].unit_hp))
+
+
 if __name__ == '__main__':
-    u1 = TeamedUnit()
+    u1 = Creature()
     u1.initiative = 9.0
     u1.name = "u1"
     u1.team = "1"
-    u2 = TeamedUnit()
+    u2 = Creature()
     u2.name = "u2"
     u2.team = "2"
 
-    manager = ActionManager()
+    manager = DamageManager()
     manager.add_unit(u1)
     manager.add_unit(u2)
 
